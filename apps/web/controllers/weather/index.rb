345f73946e418f509b109e5c674e9ff20243c55b
Justin Leveck <jleveck@gmail.com>
2018-09-11 08:01:20 -0700

    Remove comment

--- apps/web/controllers/weather/index.rb
+++ apps/web/controllers/weather/index.rb
@@ -15,7 +15,6 @@ module Web::Controllers::Weather
       url = "http://api.openweathermap.org/data/2.5/weather?q=#{URI.encode(city)}&units=imperial&APPID=#{ENV['OPEN_WEATHER_MAP_APPID']}"
       uri = URI(url)
       response = Net::HTTP.get(uri)
-      # response = "{\"coord\":{\"lon\":-118,\"lat\":33.68},\"weather\":[{\"id\":800,\"main\":\"Clear\",\"description\":\"clear sky\",\"icon\":\"01d\"}],\"base\":\"stations\",\"main\":{\"temp\":82.51,\"pressure\":1011,\"humidity\":62,\"temp_min\":71.06,\"temp_max\":95},\"visibility\":16093,\"wind\":{\"speed\":11.41,\"deg\":210},\"clouds\":{\"all\":1},\"dt\":1536612960,\"sys\":{\"type\":1,\"id\":485,\"message\":0.0152,\"country\":\"US\",\"sunrise\":1536586360,\"sunset\":1536631438},\"id\":5358705,\"name\":\"Huntington Beach\",\"cod\":200}"
       @city = JSON.parse(response)
     end
   end
